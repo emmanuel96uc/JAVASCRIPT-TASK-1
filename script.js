a1a72172e7806a5e623a256818bb12339d3fe02c
Aguagwa Emmanuel Uchenna <emmanuel96uc@yahoo.com>
2020-03-26 22:44:19 +0000

    added folders

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let name = 'Aguagwa Emmanuel Uchenna';
+console.log(name);
+let courses = ['HTML', 'CSS', 'JAVASCRIPT'];
+console.log (courses);
+
+for (let i = 1; i <= 200; i++) {
+if (i % 2 !==0) console.log(i);
+}
